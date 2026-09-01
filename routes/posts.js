25209a74144212402f04992009d320c65e7c7a64
Satyanshu Bhardwaj <satyanshu.bhardwaj123@gmail.com>
2026-09-01 17:18:51 +0000

    increased image upload file size to 10 mb

--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,7 @@ const uploadBufferToCloudinary = require("../utils/uploadToCloudinary");
   
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) cb(null, true);
     else cb(new Error("Only image files are allowed"), false);
